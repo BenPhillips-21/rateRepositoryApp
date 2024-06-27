e0b834aed68faa0322a3ab09316f554c34943ed9
Ben Phillips <pben012@gmail.com>
2024-06-27 13:33:44 +1000

    added picker allowing user to order repos

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,41 +1,100 @@
-import { FlatList, Text, View, StyleSheet } from 'react-native';
-import { RepositoryItem } from './RepositoryItem'
-import { useState, useEffect } from 'react';
+import { FlatList, View, StyleSheet, Pressable } from 'react-native';
+import { RepositoryItem } from './RepositoryItem';
+import { useState } from 'react';
 import useRepositories from '../hooks/UseRepos';
+import { Picker } from '@react-native-picker/picker';
+import Text from './Text';
 
 const styles = StyleSheet.create({
   separator: {
     height: 10,
   },
+  container: {
+    paddingTop: 50,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  pickerContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    zIndex: 1000, // Ensures the menu appears in front of everything else
+    backgroundColor: 'white',
+  },
+  pickerStyle: {
+    width: '100%',
+  },
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({repositories}) => {
+const LePicker = ({ open, setOpen, setOrderBy, setOrderDirection }) => {
+  const [leOrder, setLeOrder] = useState('');
 
+  if (!open) return null;
+
+  return (
+    <View style={styles.pickerContainer}>
+      <Pressable onPress={() => setOpen(!open)}>
+        <Text>Order</Text>
+      </Pressable>
+      <Picker
+        style={styles.pickerStyle}
+        selectedValue={leOrder}
+        onValueChange={(itemValue) => {
+          const arrayValue = JSON.parse(itemValue);
+          setLeOrder(itemValue);
+          setOrderBy(arrayValue[0]);
+          setOrderDirection(arrayValue[1]);
+        }}
+      >
+        <Picker.Item label="Latest Repositories" value={JSON.stringify(["CREATED_AT", "DESC"])} />
+        <Picker.Item label="Highest Rated Repositories" value={JSON.stringify(["RATING_AVERAGE", "DESC"])} />
+        <Picker.Item label="Lowest Rated Repositories" value={JSON.stringify(["RATING_AVERAGE", "ASC"])} />
+      </Picker>
+    </View>
+  );
+};
+
+export const RepositoryListContainer = ({ repositories, orderBy, setOrderBy, orderDirection, setOrderDirection }) => {
   const repositoryNodes = repositories
     ? repositories.edges.map(edge => edge.node)
     : [];
 
+    const [open, setOpen] = useState(false);
+
   return (
     <View>
-        <FlatList
-            data={repositoryNodes}
-            ItemSeparatorComponent={ItemSeparator}
-            renderItem={({item}) => <RepositoryItem item={item} />}
-            keyExtractor={item => item.id}
-        />
+      <Pressable onPress={() => setOpen(!open)}>
+        <Text>Order</Text>
+      </Pressable>
+      <LePicker open={open} setOpen={setOpen} setOrderBy={setOrderBy} setOrderDirection={setOrderDirection} />
+      <FlatList
+        data={repositoryNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        renderItem={({ item }) => <RepositoryItem item={item} />}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
 };
 
 const RepositoryList = () => {
-  const [orderBy, setOrderBy] = useState(null)
-  const [orderDirection, setOrderDirection] = useState(null)
+  const [orderBy, setOrderBy] = useState(null);
+  const [orderDirection, setOrderDirection] = useState(null);
 
-  const { repositories } = useRepositories(orderBy, orderDirection)
+  const { repositories } = useRepositories(orderBy, orderDirection);
 
-  return <RepositoryListContainer repositories={repositories} />;
+  return (
+    <RepositoryListContainer 
+      repositories={repositories} 
+      orderBy={orderBy} 
+      setOrderBy={setOrderBy} 
+      orderDirection={orderDirection} 
+      setOrderDirection={setOrderDirection} 
+    />
+  );
 };
 
 export default RepositoryList;
